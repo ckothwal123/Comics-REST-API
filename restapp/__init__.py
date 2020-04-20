1994cf659b39ddec7d5b5b7c1814293101f55abf
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-04-19 23:19:14 -0500

    Finished database setup

---
+++ restapp/__init__.py
@@ -0,0 +1,9 @@
+import pymysql
+
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.mysql", "NAME": "comics",}}
+
+# Fake PyMySQL's version and install as MySQLdb
+# https://adamj.eu/tech/2020/02/04/how-to-use-pymysql-with-django/
+pymysql.version_info = (1, 4, 2, "final", 0)
+pymysql.install_as_MySQLdb()
