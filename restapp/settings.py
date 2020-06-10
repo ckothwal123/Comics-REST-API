109afc39733b1a3b3752c79806ebd2900b9e34b8
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-09 23:11:14 -0500

    fixed static files error for heroku

--- restapp/settings.py
+++ restapp/settings.py
@@ -157,3 +157,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 15,
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
