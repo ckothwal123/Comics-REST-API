e3fbe1b8c63221dabad61994636aa26b7e72bd2f
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-09 23:04:38 -0500

    Updated requirements file

--- restapp/__init__.py
+++ restapp/__init__.py
@@ -3,14 +3,14 @@ from __future__ import absolute_import, unicode_literals
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
-import pymysql
+# import pymysql
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.mysql", "NAME": "comics",}}
+# DATABASES = {"default": {"ENGINE": "django.db.backends.mysql", "NAME": "comics",}}
 
-# Fake PyMySQL's version and install as MySQLdb
-# https://adamj.eu/tech/2020/02/04/how-to-use-pymysql-with-django/
-pymysql.version_info = (1, 4, 2, "final", 0)
-pymysql.install_as_MySQLdb()
+# # Fake PyMySQL's version and install as MySQLdb
+# # https://adamj.eu/tech/2020/02/04/how-to-use-pymysql-with-django/
+# pymysql.version_info = (1, 4, 2, "final", 0)
+# pymysql.install_as_MySQLdb()
 
 
 # Configuration of celery
