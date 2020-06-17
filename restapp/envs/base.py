6baeaaec9dd425213122d5b83a65d001c9e27fad
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-17 12:51:00 -0500

    fixed issues with git cache

--- restapp/envs/base.py
+++ restapp/envs/base.py
@@ -17,20 +17,6 @@ from celery.schedules import crontab
 
 CELERY_BROKER_URL = "amqp://guest:guest@localhost"
 
-# #Celery beat is used to schedule the tasks
-# CELERY_BEAT_SCHEDULE = {
-#     'task-scrape-awkward-yeti': {
-#         'task': 'webscrape.tasks.scrape_awkward_yeti',
-#         'schedule': crontab(minute='*/5'),
-#         'args': (4, 5)
-#     },
-# }
-
-#: Only add pickle to this list if your broker is secured
-#: from unwanted access (see userguide/security.html)
-# CELERY_ACCEPT_CONTENT = ['json']
-# CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
-# CELERY_TASK_SERIALIZER = 'json'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- restapp/envs/production.py
+++ restapp/envs/production.py
@@ -1,7 +1,12 @@
 from restapp.envs.base import *
 import dj_database_url
+import django_heroku
 
+django_heroku.settings(locals())
 ALLOWED_HOSTS = ['*']
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 SECRET_KEY = "(3gbc8!%m4t#t22(*edr=93z!3&uwpp!k2=-k-2&vf7g=5vo48"
 DEBUG = False
