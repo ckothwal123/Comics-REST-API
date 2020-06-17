474af8917a237468d6051216a707056c698384f7
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-17 16:28:54 -0500

    Changed secret key

--- restapp/envs/production.py
+++ restapp/envs/production.py
@@ -8,8 +8,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-SECRET_KEY = "(3gbc8!%m4t#t22(*edr=93z!3&uwpp!k2=-k-2&vf7g=5vo48"
+SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
