65730b2de235df125f69ce8813656aaedba66c40
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-17 12:07:22 -0500

    Testing database

--- restapp/envs/production.py
+++ restapp/envs/production.py
@@ -1,20 +1,21 @@
 from restapp.envs.base import *
-
+import dj_database_url
 
 ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = "(3gbc8!%m4t#t22(*edr=93z!3&uwpp!k2=-k-2&vf7g=5vo48"
 DEBUG = False
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-DATABASES = {
+# DATABASES = {
 
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        # "NAME": "comics",
-        # "USER": "postgres",
-        # "PASSWORD": "testingdb",
-        # "HOST": "localhost",
-        # "PORT": "5432",
-        # "TEST": {"NAME": "mytestdatabase",},
-    },
-}
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         # "NAME": "comics",
+#         # "USER": "postgres",
+#         # "PASSWORD": "testingdb",
+#         # "HOST": "localhost",
+#         # "PORT": "5432",
+#         # "TEST": {"NAME": "mytestdatabase",},
+#     },
+# }
