e9ff781a112bc7939e6a6c42ea556453daaf6c76
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-17 11:59:39 -0500

    Added prod settings

--- restapp/envs/production.py
+++ restapp/envs/production.py
@@ -1,6 +1,8 @@
 from restapp.envs.base import *
 
 
+ALLOWED_HOSTS = ['*']
+
 SECRET_KEY = "(3gbc8!%m4t#t22(*edr=93z!3&uwpp!k2=-k-2&vf7g=5vo48"
 DEBUG = False
 
@@ -8,11 +10,11 @@ DATABASES = {
 
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "comics",
-        "USER": "postgres",
-        "PASSWORD": "testingdb",
-        "HOST": "localhost",
-        "PORT": "5432",
-        "TEST": {"NAME": "mytestdatabase",},
+        # "NAME": "comics",
+        # "USER": "postgres",
+        # "PASSWORD": "testingdb",
+        # "HOST": "localhost",
+        # "PORT": "5432",
+        # "TEST": {"NAME": "mytestdatabase",},
     },
 }
